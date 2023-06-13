9476e4d76d23960abc7fe748ba0449af73771cb9
Paulo Santos <paulo.santos@buser.com.br>
2023-06-13 17:17:35 -0300

    creates class 2

---
+++ 2-Comments/main.cpp
@@ -0,0 +1,12 @@
+/*
+  apesar de eu já ter deixado uns comments na última aula,
+  ele cria essa aula pra ensinar a usar comentários
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
